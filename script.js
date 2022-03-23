6b9b95840dc7a8b4d6c683a8cbab10fdd080d289
Pramit Majumdar <pramitmajumdar23555@gmail.com>
2022-03-23 20:44:49 +0530

    Product,Index,Order,My_profile

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+//Popover Trigger
+const popoverTriggerList = [].slice.call(
+  document.querySelectorAll("[data-bs-toggle='popover']")
+);
+const popoverList = popoverTriggerList.map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
